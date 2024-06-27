72089c210a13e486480945ffbad4fd3e93f13b05
ohshitnotgood <praanto@icloud.com>
2024-06-27 02:48:26 +0200

    Added flask server

---
+++ inference/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from flask_cors import CORS
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+CORS(app=app)
+
+
+@app.post("/inference")
+def endpoint():
+    json = request.json
+    imageData = json["imData"]
+    imageData = imageData.split(",")
+    imageData = np.array(imageData, dtype=np.uint8)
+    imageData = np.reshape(imageData, (600, 600, 4))
+    data = Image.fromarray(imageData, "RGBA")
+    data = data.convert("L")
+    data = data.resize((28, 28))
+    data.save("./out/savefile.png")
+    return "ok"
